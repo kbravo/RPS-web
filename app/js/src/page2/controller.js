75cf31d3fa61102fc428b9e16f5c2f1215021ce1
jmcdoug <jmcdoug@purdue.edu>
2015-11-22 07:20:44 -0500

    venmo auth

--- app/js/src/page2/controller.js
+++ app/js/src/page2/controller.js
@@ -1,5 +1,22 @@
 var app = angular.module('myApp.page2', []);
 
-app.controller('page2Controller', ['$scope', function($scope) {
+app.controller('page2Controller', ['$scope', '$location', '$routeParams', '$window', function($scope, $location, $routeParams, $window) {
 	$scope.identify = "You are on page - 2!";
+	var CLIENT_ID = 3196;
+	
+	var index1 = 0;
+	var index2 = 0;
+	console.log($location.absUrl());
+	var url = new String($location.absUrl());
+	index1 = url.lastIndexOf("=");
+	index2 = url.lastIndexOf("#");
+	if(index1 > 0 && index2 > 0) {
+		url = url.substring(index1+1, index2);
+	}
+	console.log(url);
+
+	$scope.authenticate = function() {
+		console.log('foo');
+		$window.location.href = 'https://api.venmo.com/v1/oauth/authorize?client_id=' + CLIENT_ID + '&scope=make_payments%20access_profile&response_type=token';
+	}
 }]);
